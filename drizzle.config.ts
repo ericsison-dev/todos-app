30e6d1105a813bf8023b8db3c7652fb492e3ed55
Eric Sison <ecsison@addu.edu.ph>
2025-09-06 10:44:38 +0800

    modified drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   schema: "./db/schemas.ts",
   out: "./db/migrations",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.DATABASE_URL!,
+    // ssl: true
     // host: process.env.DB_HOST!,
     // port: parseInt(process.env.DB_PORT!),
     // user: process.env.DB_USER!,
